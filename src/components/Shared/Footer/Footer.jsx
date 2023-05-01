18074148f1f7d0534279d214d9bf983ba5c83340
AnikaBushra <anikabushra024@gmail.com>
2023-05-01 22:41:23 +0600

    footer completed

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+
 
 const Footer = () => {
     return (
-        <div className='text-center h-28 py-10 bg-black text-white'>
+        <div className='text-center h-36 py-10 bg-black text-white'>
+
+            <h3>
+                Contact us on
+            </h3>
+            <div className='flex justify-center'>
+                <FaFacebook className='m-4'></FaFacebook>
+                <FaTwitter className='m-4'></FaTwitter>
+                <FaYoutube className='m-4'></FaYoutube>
+            </div>
+
             &copy; All rights reserved.
         </div>
     );
