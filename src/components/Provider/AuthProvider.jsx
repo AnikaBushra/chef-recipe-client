324378701732f84c8a8639cbc7e2b25ee5db2f01
AnikaBushra <anikabushra024@gmail.com>
2023-05-02 17:40:40 +0600

    registation page completed

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,11 +1,21 @@
 import React, { createContext } from 'react';
+import { app } from '../../firebase/firebase.config';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 export const AuthContext = createContext(null)
 
 
 const AuthProvider = ({ children }) => {
-    const authInfo = { user: 'fsdfdsf' }
+
+    const auth = getAuth(app);
+
+    const signUpUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+
+    const authInfo = { signUpUser }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Register = () => {
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const { signUpUser } = useContext(AuthContext);
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const photoUrl = form.photoUrl.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        setError('')
+        setSuccess('')
+        if (password.length < 6) {
+            return setError('password can not be less than 6 characters')
+        }
+        signUpUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess('User created successfully')
+                form.reset()
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+        console.log(name, photoUrl, email, password);
+    }
+
+
     return (
         <div>
             <div className="hero min-h-screen bg-gray-300 ">
@@ -11,41 +41,51 @@ const Register = () => {
                     <div className="card  w-full  shadow-2xl bg-base-100">
 
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Name</span>
-                                </label>
-                                <input type="text" placeholder="Your name" className="input input-bordered"
-                                    name='name' />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Photo Url</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered"
-                                    name='photoUrl' />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="email" className="input input-bordered"
-                                    name='email' />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered"
-                                    name='password' />
+                            <form onSubmit={handleRegister}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Your name" className="input input-bordered"
+                                        name='name'
+                                        required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo Url</span>
+                                    </label>
+                                    <input type="text" placeholder="email" className="input input-bordered"
+                                        name='photoUrl'
+                                        required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" placeholder="email" className="input input-bordered"
+                                        name='email'
+                                        required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" placeholder="password" className="input input-bordered"
+                                        name='password'
+                                        required />
 
-                            </div>
-                            <div className="form-control mt-4">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
+                                </div>
+                                <p className='text-red-500'>{error}</p>
+                                <p className='text-green-500-500'>
+                                    {success}
+                                </p>
+                                <div className="form-control mt-4">
+                                    <button className="btn btn-primary">Register</button>
+                                </div>
+                            </form>
 
                             <label className="label">
-                                <p >Already registered??<Link className=" text-xl text-violet-500" to={'/register'}>Login here.</Link></p>
+                                <p >Already registered??<Link className=" text-xl text-violet-500" to={'/login'}>Login here.</Link></p>
                             </label>
                         </div>
 
